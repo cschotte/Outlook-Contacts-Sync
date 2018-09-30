8f40c65cc399eb1874a9bd3d8ec169a13b53ac2e
Clemens Schotte <clemens@navatron.com>
2018-09-30 09:31:20 +0200

    Added About link

--- Source/Views/MainView.xaml.cs
+++ Source/Views/MainView.xaml.cs
@@ -1,19 +1,8 @@
 ﻿using NavaTron.Outlook.Contacts.Sync.Controllers;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace NavaTron.Outlook.Contacts.Sync.Views
 {
@@ -28,8 +17,6 @@ namespace NavaTron.Outlook.Contacts.Sync.Views
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            Title += Assembly.GetExecutingAssembly().GetName().Version.ToString();
-
             worker.DoWork += Worker_DoWork;
             worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
             worker.ProgressChanged += Worker_ProgressChanged;
@@ -97,5 +84,11 @@ namespace NavaTron.Outlook.Contacts.Sync.Views
             SettingsButton.IsEnabled = true;
         }
 
+        private void AboutHyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+
+            e.Handled = true;
+        }
     }
 }
