d1f57004792e6b3f062cb69ce38a9ef078dfbad0
Clemens Schotte <clemens@navatron.com>
2019-02-27 11:20:50 +0100

    Removed Windows Store

--- Source/Properties/Settings.Designer.cs
+++ Source/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace NavaTron.Outlook.Contacts.Sync.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,8 @@ namespace NavaTron.Outlook.Contacts.Sync.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        [global::System.Configuration.DefaultSettingValueAttribute("(|(physicaldeliveryofficename=SCHIPHOL*)(department=NL*)(department=EQ-NL*)(depar" +
+            "tment=EQ-EQ-NL*)(company=NETHERLANDS))")]
         public string Filter {
             get {
                 return ((string)(this["Filter"]));
